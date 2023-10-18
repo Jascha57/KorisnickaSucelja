82f7e451ab137eea5bf0262eb441655fe7ffeee5
dmarti01 <dmarti01@fesb.hr>
2023-10-18 15:48:58 +0200

    vjezbanje

--- semester-project/app/showcase/page.tsx
+++ semester-project/app/showcase/page.tsx
@@ -1,7 +1,7 @@
 export default function Showcase() {
     return (
         <main>
-          <h1 className="flex justify-center p-14 text-4xl" style={{color: "black",}}>Showcase page</h1>
+          <h1 className="flex justify-center p-14 text-4xl" style={{color: "black",}}>Showcase </h1>
         </main>
     )
 }
