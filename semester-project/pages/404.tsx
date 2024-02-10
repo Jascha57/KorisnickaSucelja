3dab4841020755169522ee23f0918ed05096fc0c
Jascha57 <jakov.petric.student@gmail.com>
2024-02-10 16:41:39 +0100

    Refactor 404 page with Next.js router

--- semester-project/pages/404.tsx
+++ semester-project/pages/404.tsx
@@ -1,28 +1,46 @@
-import Link from 'next/link'
+import { useRouter } from "next/navigation";
 
 export default function NotFound() {
-    return(
-        <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
-            <div className="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
-                <div className="relative">
-                    <div className="absolute">
-                        <div className="">
-                            <h1 className="my-2 text-gray-800 font-bold text-2xl">
-                                Looks like you have found the
-                                doorway to the great nothing
-                            </h1>
-                            <p className="my-2 text-gray-800">Sorry about that! Please visit our hompage to get where you need to go.</p>
-                            <button className="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">Take me there!</button>
-                        </div>
-                    </div>
-                    <div>
-                        <img src="https://i.ibb.co/G9DC8S0/404-2.png" />
+    const router = useRouter();
+
+    const goBack = () => {
+        router.back();
+    };
+
+    const goToHome = () => {
+        router.push('/');
+    };
+
+    return( 
+        <section className="bg-white dark:bg-gray-900 ">
+            <div className="container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12">
+                <div className="wf-ull lg:w-1/2">
+                    <p className="text-sm font-medium text-blue-500 dark:text-blue-400">404 error</p>
+                    <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">Page not found</h1>
+                    <p className="mt-4 text-gray-500 dark:text-gray-400">Sorry, the page you are looking for does not exist. Here are some helpful links:</p>
+
+                    <div className="flex items-center mt-6 gap-x-3">
+                        <button 
+                            onClick={goBack}
+                            className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700"
+                            >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 rtl:rotate-180">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                            </svg>
+                            <span>Go back</span>
+                            </button>
+                            <button 
+                            onClick={goToHome}
+                            className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600"
+                            >
+                            Take me home
+                        </button>
                     </div>
                 </div>
+                <div className="relative w-full mt-12 lg:w-1/2 lg:mt-0">
+                    <img className="w-full max-w-lg lg:mx-auto" src="/images/components/illustration.svg" alt=""></img>
+                </div>
             </div>
-            <div>
-                <img src="https://i.ibb.co/ck1SGFJ/Group.png" />
-            </div>
-        </div>
+        </section>
     )
 }
