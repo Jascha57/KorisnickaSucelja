2b9ce4d709e0eab0c86c56f02d0e222f99b7fa99
Jascha57 <jakov.petric.student@gmail.com>
2024-02-10 16:27:12 +0100

    Update 404 page layout and content

--- semester-project/pages/404.tsx
+++ semester-project/pages/404.tsx
@@ -2,27 +2,27 @@ import Link from 'next/link'
 
 export default function NotFound() {
     return(
-        <div className="not-found">
-        <h1>404</h1>
-        <h2>OOOPS! This page is on a coffee break, probably sipping java with other rebellious pages. 
-            While we send out a search party, enjoy the chaos. 
-            In the meantime, tell a joke or ponder life&aposs great mysteries. 
-            If the page doesn&apost return, blame it on cosmic typos and consider this your internet scavenger hunt training.</h2>
-            <p>Redirecting to the <Link href="/">Homepage</Link></p>
-
-            <style jsx>{`
-            .not-found {
-                background: #fff;
-                padding: 30px;
-                box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
-                transform: rotateZ(-1deg);
-            }
-            h1 {
-                font-size: 3em
-            }
-            `
-            }
-            </style>
-    </div>
+        <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
+            <div className="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
+                <div className="relative">
+                    <div className="absolute">
+                        <div className="">
+                            <h1 className="my-2 text-gray-800 font-bold text-2xl">
+                                Looks like you've found the
+                                doorway to the great nothing
+                            </h1>
+                            <p className="my-2 text-gray-800">Sorry about that! Please visit our hompage to get where you need to go.</p>
+                            <button className="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">Take me there!</button>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="https://i.ibb.co/G9DC8S0/404-2.png" />
+                    </div>
+                </div>
+            </div>
+            <div>
+                <img src="https://i.ibb.co/ck1SGFJ/Group.png" />
+            </div>
+        </div>
     )
 }
